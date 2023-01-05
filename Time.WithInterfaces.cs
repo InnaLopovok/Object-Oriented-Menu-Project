249fe95f4dec801843a58442086be8e48d9057d8
InnaLopovok <101043653+InnaLopovok@users.noreply.github.com>
2023-01-05 13:13:09 +0200

    Add files via upload

---
+++ Time.WithInterfaces.cs
@@ -0,0 +1,17 @@
+﻿namespace WithInterfaces
+{
+    using Ex4_Inna_Adam;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Time : IClickable
+    {
+        public void ItemAction()
+        {
+            Console.WriteLine("Current time: {0}", DateTime.Now.ToString("HH:mm:ss tt"));
+        }
+    }
+}
